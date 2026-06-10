979d3289eccb609c4b2dace6465b6b5666335974
eslamhany71-lgtm <eslamhany71@gmail.com>
2026-06-10 17:37:41 +0300

    Update patient-profile.js

--- js/patient-profile.js
+++ js/patient-profile.js
@@ -11,9 +11,6 @@ try {
     }
 
     let clinicId = urlParams.get('clinicId') || sessionStorage.getItem('clinicId') || localStorage.getItem('clinicId');
-    console.log("PATIENT ID =", patientId);
-console.log("CLINIC ID =", clinicId);
-console.log("BRANCH ID =", currentBranchId);
     if (!clinicId && window.parent) {
         try { clinicId = window.parent.sessionStorage.getItem('clinicId'); } catch(e) {}
     }
